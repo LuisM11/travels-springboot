d4b5f5619637f38c4ea1c861981a6f17c496c541
LuisM11 <lumarinb@unal.edu.co>
2023-06-25 23:10:10 -0500

    fly service

---
+++ src/main/java/com/example/travels/infraestructure/services/FlyService.java
@@ -0,0 +1,73 @@
+package com.example.travels.infraestructure.services;
+
+import com.example.travels.api.models.responses.FlyResponse;
+import com.example.travels.domain.entities.jpa.FlyEntity;
+import com.example.travels.domain.repositories.jpa.FlyRepository;
+import com.example.travels.infraestructure.abstract_services.IFlyService;
+import com.example.travels.util.constants.CacheConstants;
+import com.example.travels.util.enums.SortType;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@Service
+@AllArgsConstructor
+public class FlyService implements IFlyService {
+
+    private final FlyRepository flyRepository;
+
+    @Override
+    public Page<FlyResponse> realAll(Integer page, Integer size, SortType sortType) {
+
+        PageRequest pageRequest = null;
+        switch (sortType) {
+            case NONE -> pageRequest = PageRequest.of(page, size);
+            case LOWER -> pageRequest = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).ascending());
+            case UPPER -> pageRequest = PageRequest.of(page, size, Sort.by(FIELD_BY_SORT).descending());
+        }
+        return this.flyRepository.findAll(pageRequest).map(this::entityToResponse);
+    }
+
+    @Override
+    @Cacheable(value = CacheConstants.FLY_CACHE_NAME)
+    public Set<FlyResponse> readLessPrice(BigDecimal price) {
+        return this.flyRepository.selectLessPrice(price)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    @Cacheable(value = CacheConstants.FLY_CACHE_NAME)
+    public Set<FlyResponse> readBetweenPrices(BigDecimal min, BigDecimal max) {
+        return this.flyRepository.selectBetweenPrice(min, max)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    @Cacheable(value = CacheConstants.FLY_CACHE_NAME)
+    public Set<FlyResponse> readByOriginDestiny(String origen, String destiny) {
+        return this.flyRepository.selectOriginDestiny(origen, destiny)
+                .stream()
+                .map(this::entityToResponse)
+                .collect(Collectors.toSet());
+    }
+
+    private FlyResponse entityToResponse(FlyEntity entity) {
+        FlyResponse response = new FlyResponse();
+        BeanUtils.copyProperties(entity, response);
+        return response;
+    }
+}
