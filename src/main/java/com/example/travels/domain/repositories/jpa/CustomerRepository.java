1307c6051efe258f298d1eafab63d247e43438ea
LuisM11 <lumarinb@unal.edu.co>
2023-07-01 14:48:08 -0500

    interface services: customer and catalog

--- src/main/java/com/example/travels/domain/repositories/jpa/CustomerRepository.java
+++ src/main/java/com/example/travels/domain/repositories/jpa/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.example.travels.domain.repositories.jpa;
 
-
 import com.example.travels.domain.entities.jpa.CustomerEntity;
 import org.springframework.data.repository.CrudRepository;
 
