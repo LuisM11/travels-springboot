1bad74ae089066ffbf362c8328a7b46afe714db9
LuisM11 <lumarinb@unal.edu.co>
2023-07-02 23:33:40 -0500

    Customer helper -> increase hours depending on each service

---
+++ src/main/java/com/example/travels/infraestructure/helpers/CustomerHelper.java
@@ -0,0 +1,24 @@
+package com.example.travels.infraestructure.helpers;
+
+import com.example.travels.domain.repositories.jpa.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Component
+@AllArgsConstructor
+public class CustomerHelper {
+
+    private final CustomerRepository customerRepository;
+
+    public void incrase(String customerId, Class<?> type) {
+        var customerToUpdate = this.customerRepository.findById(customerId).orElseThrow();
+        switch (type.getSimpleName()) {
+            case "TourService" -> customerToUpdate.setTotalTours(customerToUpdate.getTotalTours() + 1);
+            case "TicketService" -> customerToUpdate.setTotalFlights(customerToUpdate.getTotalFlights() + 1);
+            case "ReservationService" -> customerToUpdate.setTotalLodgings(customerToUpdate.getTotalLodgings() + 1);
+        }
+        this.customerRepository.save(customerToUpdate);
+    }
+}
