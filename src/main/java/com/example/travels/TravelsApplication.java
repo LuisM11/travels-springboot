3647dcee2be41bb187818a1a2320c9f1402f9adf
LuisM11 <lumarinb@unal.edu.co>
2023-07-08 23:57:04 -0500

    testing with mock data

--- src/main/java/com/example/travels/TravelsApplication.java
+++ src/main/java/com/example/travels/TravelsApplication.java
@@ -1,13 +1,21 @@
 package com.example.travels;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
-public class TravelsApplication {
+public class TravelsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelsApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Best Travel Application");
+    }
 }
